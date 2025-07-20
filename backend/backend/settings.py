0b68985e54744f2f7f6adf3a1eb365742ca803f2
Koydan Olesya <okojdan@yandex.ru>
2025-07-20 20:38:10 +0700

    flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,7 +1,7 @@
-from pathlib import Path
 import os
-from dotenv import load_dotenv
+from pathlib import Path
 
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['89.169.167.162', '127.0.0.1', 'localhost', 'olesyakoydan.zapto.org'] 
+ALLOWED_HOSTS = ['89.169.167.162', '127.0.0.1', 'localhost', 'olesyakoydan.zapto.org']
 
 
 # Application definition
